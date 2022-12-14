cd3f1ae6355e58b61f23b6018dcc04faf66e3b7e
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-14 09:15:28 +0200

    settings

--- app/app.py
+++ app/app.py
@@ -1,7 +1,9 @@
 from aiohttp import web
 from src.routes import setup_routes
+from src.settings import config
 
 
 app = web.Application()
 setup_routes(app)
+app['config'] = config
 web.run_app(app)
